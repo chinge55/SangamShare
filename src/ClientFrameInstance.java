6533e3c7f1ad46a51c9af6390f9ec51a759c85c3
Sangam Khanal <sangamkhnl@gmail.com>
2019-07-27 20:13:11 +0545

    Chat app done, File Sharer Remain

--- src/ClientFrameInstance.java
+++ src/ClientFrameInstance.java
@@ -1,31 +1,90 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class ClientFrameInstance{
     static JTextField textfield1, textfield2;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    public void chatSelected()
+    {
+        JFrame f = new JFrame(DatabaseConnector.username);
+        f.setSize(390, 300);
+        f.setLocation(100, 150);
+        //make sure it quits when x is clicked
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //set look and feel
+        f.setDefaultLookAndFeelDecorated(true);
+        JLabel labelM = new JLabel("Message to Send to"+DatabaseConnector.username);
+        labelM.setBounds(50, 50, 200, 30);
+        JTextField motto1 = new JTextField();
+        //set size of the text box
+        motto1.setBounds(50, 100, 200, 30);
+        //add elements to the frame
+        JButton jButton = new JButton("Send TEXT");
+        jButton.setBounds(50, 150, 100, 30);
+        f.add(jButton);
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    dataOutputStream.writeUTF(motto1.getText());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.out.println("error in sending text");
+                }
 
+            }
+        });
+        f.add(labelM);
+        f.add(motto1);
+        f.setLayout(null);
+        f.setVisible(true);
+        while(true)
+        {
+            String inputString = null;
+            try {
+                inputString = this.dataInputStream.readUTF();
+            } catch (IOException e) {
+               // e.printStackTrace();
+                System.out.println("Client Disconnected");
+            }
+            if(inputString.equals("logout"))
+                break;
+            else
+                System.out.println(DatabaseConnector.username+ ":"+inputString);
+
+        }
+    }
     public ClientFrameInstance(DataInputStream dataInputStream, DataOutputStream dataOutputStream)
     {
-        JFrame f = new JFrame("Text Field Examples");
-        f.getContentPane().setLayout(new FlowLayout());
-        textfield1 = new JTextField("Sent Message",10);
-        textfield2 = new JTextField("Received Message",10);
-        //textfield3 = new JTextField("Text field 3",10);
-        f.getContentPane().add(textfield1);
-        f.getContentPane().add(textfield2);
         try {
             dataOutputStream.writeUTF("What do you wanna do?");
             dataOutputStream.writeUTF("1. Chat \n 2. Send files\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //f.getContentPane().add(textfield3);
+            int numbcheck;
+            numbcheck = Integer.valueOf(dataInputStream.readUTF());
+            this.dataInputStream = dataInputStream;
+            this.dataOutputStream = dataOutputStream;
+            switch (numbcheck)
+            {
+                case 1:
+                    chatSelected();
+                    break;
+                case 2:
+                    break;
+                default:
 
-        f.pack();
-        f.setVisible(true);
+
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Client Disconnected");
+        }
+
     }
 }
 
